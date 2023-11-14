78e0241f475671fa85d01e57fe767f374589c938
BrzozowskiPawel <pawelbrzozowskigplus@gmail.com>
2023-11-14 20:24:35 +0100

    Some clean ups

--- WeatherApp/HomeViewController/HomeViewController.swift
+++ WeatherApp/HomeViewController/HomeViewController.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 class HomeViewController: UIViewController {
-    private let weatherSummaryStack = UIStackView()
+    private let summaryStack = UIStackView()
     private let emojiLabel = UILabel()
     private let degreesLabel = UILabel()
     private let descriptionLabel = UILabel()
@@ -16,33 +16,52 @@ class HomeViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        weatherSummaryStack.axis = .vertical
-        weatherSummaryStack.alignment = .center
-        weatherSummaryStack.spacing = 8
+        setup()
+    }
+    
+    private func setup() {
+        navigationSetup()
+        summaryStackSetup()
+    }
+    
+    private func summaryStackSetup() {
+        summaryStack.axis = .vertical
+        summaryStack.alignment = .center
+        summaryStack.spacing = 8
         view.backgroundColor = .white
 
         emojiLabel.text = "⛅️"
         degreesLabel.text = "17 °C"
         descriptionLabel.text = "Partly Cloudy"
 
-        weatherSummaryStack.addArrangedSubview(emojiLabel)
-        weatherSummaryStack.addArrangedSubview(degreesLabel)
-        weatherSummaryStack.addArrangedSubview(descriptionLabel)
+        summaryStack.addArrangedSubview(emojiLabel)
+        summaryStack.addArrangedSubview(degreesLabel)
+        summaryStack.addArrangedSubview(descriptionLabel)
 
-        view.addSubview(weatherSummaryStack)
-        weatherSummaryStack.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(summaryStack)
+        summaryStack.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
-            weatherSummaryStack.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            weatherSummaryStack.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+            summaryStack.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            summaryStack.centerYAnchor.constraint(equalTo: view.centerYAnchor)
         ])
-
+    }
+    
+    private func navigationSetup() {
         navigationItem.title = "Warsaw Poland"
-        let barButtonImage = UIImage(systemName: "line.3.horizontal")
-        let listButton = UIBarButtonItem(image: barButtonImage, style: .plain, target: self, action: #selector(listButtonTapped))
+        let listImage = UIImage(systemName: "line.3.horizontal")
+        let listButton = UIBarButtonItem(image: listImage, style: .plain, target: self, action: #selector(listButtonTapped))
         navigationItem.rightBarButtonItem = listButton
+        
+        let locationImage = UIImage(systemName: "location")
+        let locationButton = UIBarButtonItem(image: locationImage, style: .plain, target: self, action: #selector(locationButtonTapped))
+        navigationItem.leftBarButtonItem = locationButton
     }
 
     @objc func listButtonTapped() {
         print("Add button tapped!")
+    }
+    
+    @objc func locationButtonTapped() {
+        print("Location button tapped!")
     }
 }
